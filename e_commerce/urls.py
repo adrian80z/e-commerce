887eb450430594371b7a02d9e5b026ebde9df59c
Adrian <adrian.ziolkowski@gmail.com>
2020-04-12 23:38:09 +0100

    logout function view added to urls

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -19,5 +19,6 @@ from accounts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
+    url(r'^$', views.index, name="index"),
+    url(r'^accounts/logout/$', views.logout, name="logout"),
 ]
