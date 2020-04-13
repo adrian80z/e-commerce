ae1ca29e1dc7f68fbe03de7b0eac125a1ecb16a1
Adrian <adrian.ziolkowski@gmail.com>
2020-04-13 22:21:23 +0100

    login authentication code added

--- accounts/views.py
+++ accounts/views.py
@@ -18,6 +18,22 @@ def logout(request):
 
 
 def login(request):
-    """Return login page"""
-    login_form = UserLoginForm()
+    """Return login page / user authentication"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(
+                username=request.POST['username'], password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(
+                    None, "Your username or password is incorrect!")
+    else:
+        login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
