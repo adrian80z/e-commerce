e8d5a9bef27125d38fd532d24a0586e92d7020a6
Adrian <adrian.ziolkowski@gmail.com>
2020-04-16 15:17:53 +0100

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254, unique=True,
+                            default='')
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    system = models.CharField(max_length=50, blank=True, null=True)
+    processor = models.CharField(max_length=50, blank=True, null=True)
+    ram = models.CharField(max_length=50, blank=True, null=True)
+    battery_life = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
