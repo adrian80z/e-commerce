c5bd3f26a1906d9242cb1a609a7ff63ddd22b5af
Adrian <adrian.ziolkowski@gmail.com>
2020-04-17 22:54:37 +0100

    Product model field name changed

--- products/models.py
+++ products/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     image = models.ImageField(upload_to='images', blank=True, null=True)
-    system = models.CharField(max_length=50, blank=True, null=True)
+    graphics = models.CharField(max_length=50, blank=True, null=True)
     processor = models.CharField(max_length=50, blank=True, null=True)
     ram = models.CharField(max_length=50, blank=True, null=True)
     battery_life = models.CharField(max_length=50, blank=True, null=True)
