520423379fc75bccc8bf2becca7008121e14a2de
Adrian <adrian.ziolkowski@gmail.com>
2020-04-17 22:55:19 +0100

    views added for product and product details

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
 
 # Create your views here.
+
+
+def get_products(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+
+
+def product_details(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "product_details.html", {"product": product})
