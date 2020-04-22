e605fdd0f220c977c8e717f9e945aeab53fcc349
Adrian <adrian.ziolkowski@gmail.com>
2020-04-22 16:08:39 +0100

    checkout url included

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from accounts import views as accounts_view
 from products import views as products_view
 from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 from search import urls as urls_search
 from django.views.static import serve
 from .settings import MEDIA_ROOT
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^accounts/register/$', accounts_view.registration, name="registration"),
     url(r'^products/', include('products.urls')),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
