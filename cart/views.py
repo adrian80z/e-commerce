cc1082b37a19f93e124284b889a5681d0048375d
Adrian <adrian.ziolkowski@gmail.com>
2020-04-22 16:10:08 +0100

    code formatting

--- cart/views.py
+++ cart/views.py
@@ -28,6 +28,5 @@ def update_cart(request, id):
         cart[id] = quantity
     else:
         cart.pop(id)
-
-    request.session['cart'] = cart
+        request.session['cart'] = cart
     return redirect(reverse('view_cart'))
