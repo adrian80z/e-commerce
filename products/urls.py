4631e35626a02c25275c2acab448c40ff34f4171
Adrian <adrian.ziolkowski@gmail.com>
2020-04-24 14:42:18 +0100

    url name update

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from .views import get_products, product_details
 
 
 urlpatterns = [
-    url(r'^$', get_products, name='index'),
+    url(r'^$', get_products, name='products'),
     url(r'^(?P<product_id>[0-9]+)/$',
         product_details, name="detail"),
 ]
