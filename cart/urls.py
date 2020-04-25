3382150aed7aa71615668891c301bee9ccf866d5
Adrian <adrian.ziolkowski@gmail.com>
2020-04-25 22:41:46 +0100

    url for remove product added

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, update_cart
+from .views import view_cart, add_to_cart, update_cart, remove_product
 
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
     url(r'^update/(?P<id>\d+)', update_cart, name='update_cart'),
+    url(r'^remove/(?P<id>\d+)', remove_product, name='remove_product'),
 ]
