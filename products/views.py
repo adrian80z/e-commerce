6c4be21b7694c2a8fb15bfeef5e14f6b08f92030
Adrian <adrian.ziolkowski@gmail.com>
2020-04-27 12:56:08 +0100

    comments added

--- products/views.py
+++ products/views.py
@@ -5,10 +5,12 @@ from .models import Product
 
 
 def get_products(request):
+    """Display all products"""
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
 
 def product_details(request, product_id):
+    """Display single product details"""
     product = get_object_or_404(Product, pk=product_id)
     return render(request, "product_details.html", {"product": product})
