48edca4833cfd8a0cd9de5b9306b95a4f81e1ed3
Adrian <adrian.ziolkowski@gmail.com>
2020-04-29 15:52:44 +0100

    add paths to aws storage location

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -152,14 +152,15 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
-AWS_STORAGE_BUCKET_NAME = 'adrians-e-commerce-app'
+AWS_STORAGE_BUCKET_NAME = 'toptech-ecommerce'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backend.s3boto3.S3BotoStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 
 STATIC_URL = '/static/'
