4d0acbc75a6fe2f89d8329159b145ddd33a8a81c
Adrian <adrian.ziolkowski@gmail.com>
2020-04-29 23:47:33 +0100

    required field change to true

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,12 +8,12 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
 
     credit_card_number = forms.CharField(
-        label='Credit card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
+        label='Credit card number', required=True)
+    cvv = forms.CharField(label='Security code (CVV)', required=True)
     expiry_month = forms.ChoiceField(
-        label='Month', choices=MONTH_CHOICES, required=False)
+        label='Month', choices=MONTH_CHOICES, required=True)
     expiry_year = forms.ChoiceField(
-        label='Year', choices=YEAR_CHOICES, required=False)
+        label='Year', choices=YEAR_CHOICES, required=True)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
