dfa8940732d9c7db71248823c40570581382de62
Adrian <adrian.ziolkowski@gmail.com>
2020-04-30 21:40:34 +0100

    credit card input fields changed to false

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,13 +8,13 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
 
     credit_card_number = forms.CharField(
-        label='Credit card number', required=True)
+        label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)',
-                          required=True, min_length="1", max_length="3")
+                          required=False, min_length="1", max_length="3")
     expiry_month = forms.ChoiceField(
-        label='Month', choices=MONTH_CHOICES, required=True)
+        label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(
-        label='Year', choices=YEAR_CHOICES, required=True)
+        label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
